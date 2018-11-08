f331d0c2fd8464d581d0bea7e9dada3bac2e8279
Andrei Chayeuski <andreichayeuski@gmail.com>
2018-11-08 09:09:25 +0300

    lab 19 completed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const bodyParser = require('body-parser');
 
 const validator = require('./validator');
@@ -9,15 +10,53 @@ app.use(bodyParser.json());
 
 app.post('/signin', (req, res) => {
 	const validationResult = validator.check(req.route.path, req.body);
-
 	if (validationResult.error) {
 		res.status(400).json({
 			succeed: false,
 			message: validationResult.error.details
 		});
-	} else {
+	}
+	else
+	{
 		res.json({ succeed: true });
 	}
 });
+
+app.post('/signup', (req, res) => {
+	const validationResult = validator.check(req.route.path, req.body);
+	if (validationResult.error) res.status(400).json({
+		succeed: false,
+		message: validationResult.error.details
+	});
+	else
+	{
+		res.json({ succeed: true });
+	}
+});
+
+app.post('/drinks', (req, res) => {
+	const validationResult = validator.check(req.route.path, req.body);
+	if (validationResult.error) res.status(400).json({
+		succeed: false,
+		message: validationResult.error.details
+	});
+	else
+	{
+		res.json({ succeed: true });
+	}
+});
+
+app.post('/recipes', (req, res) => {
+	const validationResult = validator.check(req.route.path, req.body);
+	if (validationResult.error) res.status(400).json({
+		succeed: false,
+		message: validationResult.error.details
+	});
+	else
+	{
+		res.json({ succeed: true });
+	}
+});
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
